0b42f597ecb81fdb86e8303fe1325fcbdd24ae43
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-08 16:17:51 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Ali Can Kayaaslan on 8.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
